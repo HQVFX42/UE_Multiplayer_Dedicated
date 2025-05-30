66b957318ffb69b303b228b5a766b9f506ac3cea
Carroll42 <ford6313@gmail.com>
2025-05-30 19:07:19 +0900

    add uproject

---
+++ Source/FPSTemplate/Private/Game/ShooterGameMode.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/ShooterGameMode.h"
+
+DEFINE_LOG_CATEGORY(LogShooterGameMode);
+
+AShooterGameMode::AShooterGameMode()
+{
+}
+
+void AShooterGameMode::BeginPlay()
+{
+}
---
+++ Source/FPSTemplate/Public/Game/ShooterGameMode.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Game/ShooterGameModeBase.h"
+#include "GameLiftServerSDK.h"
+
+#include "ShooterGameMode.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogShooterGameMode, Log, All);
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTEMPLATE_API AShooterGameMode : public AShooterGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AShooterGameMode();
+
+protected:
+	virtual void BeginPlay() override;
+	//virtual void Tick(float DeltaTime) override;
+	
+private:
+	FProcessParameters ProcessParameters;
+
+};
