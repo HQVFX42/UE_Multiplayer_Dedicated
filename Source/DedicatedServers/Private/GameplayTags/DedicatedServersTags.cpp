6196788a0d6070b5256dea0c3b00db83ea13e9d1
Carroll42 <ford6313@gmail.com>
2025-06-16 16:04:08 +0900

    add api data (find-or-create-game-session)

--- Source/DedicatedServers/Private/GameplayTags/DedicatedServersTags.cpp
+++ Source/DedicatedServers/Private/GameplayTags/DedicatedServersTags.cpp
@@ -5,5 +5,6 @@ namespace DedicatedServersTags
 	namespace GameSessionsAPI
 	{
 		UE_DEFINE_GAMEPLAY_TAG_COMMENT(ListFleets, "DedicatedServersTags.GameSeesionAPI.ListFleets", "List fleets resource on the Game Sessions API.");
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(FindOrCreateGameSession, "DedicatedServersTags.GameSeesionAPI.FindOrCreateGameSession", "Retrieves an ACTIVE game session, creating one if one does not exist on the Game Sessions API.");
 	}
 }
