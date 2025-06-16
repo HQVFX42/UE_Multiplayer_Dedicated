0bd786c60c40d37d641cb7409870d3742c58f7fd
Carroll42 <ford6313@gmail.com>
2025-06-16 16:47:42 +0900

    add game seesion struct

--- Source/DedicatedServers/Private/UI/HTTP/HTTPRequestTypes.cpp
+++ Source/DedicatedServers/Private/UI/HTTP/HTTPRequestTypes.cpp
@@ -26,3 +26,33 @@ void FDSListFleetsResponse::Dump() const
 		UE_LOG(LogDS, Log, TEXT("  NextToken: <empty>"));
 	}
 }
+
+void FDSGameSession::Dump() const
+{
+	UE_LOG(LogDS, Log, TEXT("FDSGameSession:"));
+	UE_LOG(LogDS, Log, TEXT("  CreationTime: %f"), CreationTime);
+	UE_LOG(LogDS, Log, TEXT("  CreatorId: %s"), *CreatorId);
+	UE_LOG(LogDS, Log, TEXT("  CurrentPlayerSessionCount: %d"), CurrentPlayerSessionCount);
+	UE_LOG(LogDS, Log, TEXT("  DnsName: %s"), *DnsName);
+	UE_LOG(LogDS, Log, TEXT("  FleetArn: %s"), *FleetArn);
+	UE_LOG(LogDS, Log, TEXT("  FleetId: %s"), *FleetId);
+
+	UE_LOG(LogDS, Log, TEXT("  GameProperties:"));
+	for (const auto& GameProperty : GameProperties)
+	{
+		UE_LOG(LogDS, Log, TEXT("    %s: %s"), *GameProperty.Key, *GameProperty.Value);
+	}
+
+	UE_LOG(LogDS, Log, TEXT("  GameSessionData: %s"), *GameSessionData);
+	UE_LOG(LogDS, Log, TEXT("  GameSessionId: %s"), *GameSessionId);
+	UE_LOG(LogDS, Log, TEXT("  IpAddress: %s"), *IpAddress);
+	UE_LOG(LogDS, Log, TEXT("  Location: %s"), *Location);
+	UE_LOG(LogDS, Log, TEXT("  MatchmakerData: %s"), *MatchmakerData);
+	UE_LOG(LogDS, Log, TEXT("  MaximumPlayerSessionCount: %d"), MaximumPlayerSessionCount);
+	UE_LOG(LogDS, Log, TEXT("  Name: %s"), *Name);
+	UE_LOG(LogDS, Log, TEXT("  PlayerSessionCreationPolicy: %s"), *PlayerSessionCreationPolicy);
+	UE_LOG(LogDS, Log, TEXT("  Port: %d"), Port);
+	UE_LOG(LogDS, Log, TEXT("  Status: %s"), *Status);
+	UE_LOG(LogDS, Log, TEXT("  StatusReason: %s"), *StatusReason);
+	UE_LOG(LogDS, Log, TEXT("  TerminationTime: %f"), TerminationTime);
+}
