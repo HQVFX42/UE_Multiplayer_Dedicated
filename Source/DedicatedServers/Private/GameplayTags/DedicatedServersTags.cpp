f158531b995897841ebe477dbfd60eef8d0b4646
Carroll42 <ford6313@gmail.com>
2025-06-16 17:50:48 +0900

    setup create player session endpoint

--- Source/DedicatedServers/Private/GameplayTags/DedicatedServersTags.cpp
+++ Source/DedicatedServers/Private/GameplayTags/DedicatedServersTags.cpp
@@ -6,5 +6,6 @@ namespace DedicatedServersTags
 	{
 		UE_DEFINE_GAMEPLAY_TAG_COMMENT(ListFleets, "DedicatedServersTags.GameSeesionAPI.ListFleets", "List fleets resource on the Game Sessions API.");
 		UE_DEFINE_GAMEPLAY_TAG_COMMENT(FindOrCreateGameSession, "DedicatedServersTags.GameSeesionAPI.FindOrCreateGameSession", "Retrieves an ACTIVE game session, creating one if one does not exist on the Game Sessions API.");
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(CreatePlayerSession, "DedicatedServersTags.GameSeesionAPI.CreatePlayerSession", "Creates a new player Session on the Game Sessions API.");
 	}
 }
